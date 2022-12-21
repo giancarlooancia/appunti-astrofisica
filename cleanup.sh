4e5afdd2b1cae25582733004371f972d6facd019
Giancarlo Oancia <g.oancia@gmail.com>
2022-12-22 00:31:42 +0100

    script cleanup

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+mv -v ./main.pdf ./appunti-astrofisica.pdf
+rm -r main.aux
+rm -r main.fdb_latexmk
+rm -r main.fls
+rm -r main.log
+rm -r main.put
+rm -r main.ptc
+rm -r main.synctex.gz
+rm -r main.toc
+rm -r main.out
+#rm -r main.ind
